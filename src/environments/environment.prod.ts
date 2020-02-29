33fab7db3bba3642e7a0e898603a49dc66e3f650
Pradhumna Pancholi <pnpancholi6629.pnp@gmail.com>
2020-02-29 17:05:39 -0500

    added setup to work with firebase, bootstrap, and basic start navbar

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAi6uEqjEPl7w9Inkd9Y3sd6037L2QeVVg",
+    authDomain: "eathealthy-2f089.firebaseapp.com",
+    databaseURL: "https://eathealthy-2f089.firebaseio.com",
+    projectId: "eathealthy-2f089",
+    storageBucket: "eathealthy-2f089.appspot.com",
+    messagingSenderId: "510983253654",
+    appId: "1:510983253654:web:1238c0827c828039449cb7"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAi6uEqjEPl7w9Inkd9Y3sd6037L2QeVVg",
+    authDomain: "eathealthy-2f089.firebaseapp.com",
+    databaseURL: "https://eathealthy-2f089.firebaseio.com",
+    projectId: "eathealthy-2f089",
+    storageBucket: "eathealthy-2f089.appspot.com",
+    messagingSenderId: "510983253654",
+    appId: "1:510983253654:web:1238c0827c828039449cb7"
+  }
 };
 
 /*
