0693449b9482fa24dd3379caf90ba4e04182f5ca
Pradhumna Pancholi <pnpancholi6629.pnp@gmail.com>
2020-03-01 21:29:59 -0500

    added products listing for admin panel

--- src/app/admin/products-form/products-form.component.ts
+++ src/app/admin/products-form/products-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core'
+import { Router } from '@angular/router'
 import { CategoryService } from 'src/app/category.service'
 import { ProductService } from 'src/app/product.service'
 
@@ -11,7 +12,10 @@ export class ProductsFormComponent implements OnInit {
 
   categories$;
 
-  constructor(private categoryService: CategoryService, private productService: ProductService) {
+  constructor(
+    private router: Router,
+    private categoryService: CategoryService,
+    private productService: ProductService) {
     this.categories$ = this.categoryService.getCategories();
   }
 
@@ -20,5 +24,7 @@ export class ProductsFormComponent implements OnInit {
 
   save(product){
     this.productService.create(product)
+    this.router.navigateByUrl['/admin/products']
   }
+
 }
