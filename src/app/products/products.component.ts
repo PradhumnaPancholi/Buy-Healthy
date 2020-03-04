de0aac4a6927804a8a098a20083f550c46a8a32f
Pradhumna Pancholi <pnpancholi6629.pnp@gmail.com>
2020-03-04 03:08:52 -0500

    added functionality to display categories

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnDestroy, OnInit } from '@angular/core'
 import { Subscription } from 'rxjs'
 
+import { CategoryService } from '../category.service'
 import { Product } from '../model/product'
 import { ProductService } from '../product.service'
 
@@ -9,15 +10,20 @@ import { ProductService } from '../product.service'
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.sass']
 })
-export class ProductsComponent implements OnInit {
+export class ProductsComponent implements OnInit, OnDestroy {
 
   products : Product[]
-  subscription : Subscription
+  productsSubscription : Subscription
+  categories$
 
-  constructor(private productService: ProductService) { }
+  constructor(
+    private productService: ProductService,
+    private categoryService: CategoryService) {
+      this.categories$ = this.categoryService.getCategories()
+    }
 
   ngOnInit(){
-    this.subscription = this.productService.getAll().subscribe(data => {
+    this.productsSubscription =  this.productService.getAll().subscribe(data => {
       this.products = data.map(e => {
           return {
             id: e.payload.key,
@@ -25,6 +31,10 @@ export class ProductsComponent implements OnInit {
           } as Product;
       });
     });
+  }
+
+  ngOnDestroy(){
+    this.productsSubscription.unsubscribe()
   }
 
 }
